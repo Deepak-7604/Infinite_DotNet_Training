0b777d4e7c70402e8875fbb6775e0ea264963397
Deepak Loganathan <deepakl@infinite.com>
2026-04-02 11:54:22 +0530

    Code Challenge1

---
+++ Code_Challenge1/EmployeeManagementSystem.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code_Challenge1
+{
+    internal class Employee
+    {
+        public int Id;
+        public string Name;
+        public string Department;
+        public double Salary;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>();
+            int choice = 0;
+
+            while (choice != 6)
+            {
+                Console.WriteLine("\n======= Employee Menu =======");
+                Console.WriteLine("1 Add New Employee");
+                Console.WriteLine("2 View all Employee");
+                Console.WriteLine("3 Search Employee By ID");
+                Console.WriteLine("4 Update Employee By details");
+                Console.WriteLine("5 Delete Employee By ID");
+                Console.WriteLine("6 Exit");
+                Console.WriteLine("=============================");
+                Console.Write("Enter choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Employee e = new Employee();
+
+                    Console.Write("Enter Employee Id: ");
+                    e.Id = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Enter Employee Name: ");
+                    e.Name = Console.ReadLine();
+
+                    Console.Write("Enter Dept: ");
+                    e.Department = Console.ReadLine();
+
+                    Console.Write("Enter Salary: ");
+                    e.Salary = Convert.ToDouble(Console.ReadLine());
+
+                    empList.Add(e);
+                    Console.WriteLine("Added Succesfully");
+                }
+                else if (choice == 2)
+                {
+                    if (empList.Count == 0)
+                    {
+                        Console.WriteLine("No data found");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < empList.Count; i++)
+                        {
+                            Console.WriteLine(empList[i].Id + " " +
+                                              empList[i].Name + " " +
+                                              empList[i].Department + " " +
+                                              empList[i].Salary);
+                        }
+                    }
+                }
+                else if (choice == 3)
+                {
+                    Console.Write("Enter Id: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    bool found = false;
+
+                    for (int i = 0; i < empList.Count; i++)
+                    {
+                        if (empList[i].Id == id)
+                        {
+                            Console.WriteLine(empList[i].Id + " " +
+                                              empList[i].Name + " " +
+                                              empList[i].Department + " " +
+                                              empList[i].Salary);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                }
+                else if (choice == 4)
+                {
+                    Console.Write("Enter Id: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    bool found = false;
+
+                    for (int i = 0; i < empList.Count; i++)
+                    {
+                        if (empList[i].Id == id)
+                        {
+                            Console.Write("New Name: ");
+                            empList[i].Name = Console.ReadLine();
+
+                            Console.Write("New Dept: ");
+                            empList[i].Department = Console.ReadLine();
+
+                            Console.Write("New Salary: ");
+                            empList[i].Salary = Convert.ToDouble(Console.ReadLine());
+
+                            Console.WriteLine("Updated Succesfully");
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                }
+                else if (choice == 5)
+                {
+                    Console.Write("Enter Id: ");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    bool found = false;
+
+                    for (int i = 0; i < empList.Count; i++)
+                    {
+                        if (empList[i].Id == id)
+                        {
+                            empList.RemoveAt(i);
+                            Console.WriteLine("Deleted sucessfully");
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        Console.WriteLine("Not found");
+                    }
+                }
+                else if (choice == 6)
+                {
+                    Console.WriteLine("Thanks for visiting Our Employes management System");
+                }
+                else
+                {
+                    Console.WriteLine("Wrong choice");
+                }
+            }
+        }
+    }
+}
---
+++ Code_Challenge1/NestedStructEmployee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Code_Challenge1
+{
+    public struct DateOfBirth
+    {
+        public int Day;
+        public int Month;
+        public int Year;
+    }
+
+    public struct EmployeeNested
+    {
+        public string Name;
+        public DateOfBirth Dob; 
+    }
+
+    class NestedStructEmployeeProgram
+    {
+        static void Main()
+        {
+            EmployeeNested[] emp = new EmployeeNested[2];
+
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.Write("Name of the employee: ");
+                emp[i].Name = Console.ReadLine();
+
+                Console.Write("Enter day of the birth: ");
+                emp[i].Dob.Day = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter month of the birth: ");
+                emp[i].Dob.Month = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter year for the birth: ");
+                emp[i].Dob.Year = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\nEmployee Details:");
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine("Name: " + emp[i].Name);
+                Console.WriteLine("DOB: " + emp[i].Dob.Day + "/" +
+                                           emp[i].Dob.Month + "/" +
+                                           emp[i].Dob.Year);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
