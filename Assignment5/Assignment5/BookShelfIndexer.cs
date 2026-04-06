82cfb4c81545b87f97da990e931c5ad40bccf3eb
Deepak Loganathan <deepakl@infinite.com>
2026-04-06 15:17:48 +0530

    Assignment5

--- Assignment5/Assignment5/BookShelfIndexer.cs
+++ Assignment5/Assignment5/BookShelfIndexer.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Assignment5
 {
-    // Books class
-    internal class Books
+    // Define the Books class
+    class Books
     {
+        // Properties
         public string BookName { get; set; }
         public string AuthorName { get; set; }
 
@@ -15,58 +20,74 @@ namespace Assignment5
             AuthorName = authorName;
         }
 
-        // Display Method
+        // Method to display book details
         public void Display()
         {
-            Console.WriteLine($"Book: {BookName}, Author: {AuthorName}");
+            Console.WriteLine("Book Name: {0}", BookName);
+            Console.WriteLine("Author Name: {0}", AuthorName);
+            Console.WriteLine();
         }
     }
 
-    // BookShelf class using Aggregation
-    internal class BookShelf
+    // Define the BookShelf class
+    class BookShelf
     {
-        private Books[] bookList = new Books[5];
+        private Books[] books; // Array to store Books objects
 
-        // Indexer
+        // Constructor
+        public BookShelf()
+        {
+            // Initialize the array to store 5 Books objects
+            books = new Books[5];
+        }
+
+        // Indexer for Books objects
         public Books this[int index]
         {
-            get
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+
+        // Method to display all books in the BookShelf
+        public void DisplayAllBooks()
+        {
+            Console.WriteLine("Books on the Bookshelf:");
+            Console.WriteLine("");
+            foreach (var book in books)
             {
-                if (index >= 0 && index < bookList.Length)
-                    return bookList[index];
-                else
-                    throw new IndexOutOfRangeException("Invalid index");
-            }
-            set
-            {
-                if (index >= 0 && index < bookList.Length)
-                    bookList[index] = value;
-                else
-                    throw new IndexOutOfRangeException("Invalid index");
+                if (book != null)
+                {
+                    book.Display();
+                }
             }
         }
     }
 
-  
-    class Program
+    class BookShelf_Test
     {
         static void Main()
         {
+            // Create an instance of BookShelf
             BookShelf shelf = new BookShelf();
 
-            // Assigning values using indexer
-            shelf[0] = new Books("The Alchemist", "Paulo Coelho");
-            shelf[1] = new Books("Wings of Fire", "A.P.J Abdul Kalam");
-            shelf[2] = new Books("Rich Dad Poor Dad", "Robert Kiyosaki");
-            shelf[3] = new Books("1984", "George Orwell");
-            shelf[4] = new Books("The Hobbit", "J.R.R. Tolkien");
+            // Assign values to the Books objects using indexer
+            shelf[0] = new Books("Book A", "Mr. ABC");
+            shelf[1] = new Books("Book B", "Mr. DEF");
+            shelf[2] = new Books("Book C", "Mrs. GHJ ");
+            shelf[3] = new Books("Book D", "Ms. KLM");
+            shelf[4] = new Books("Book E", "Mr. PQR");
 
-            // Display books
-            Console.WriteLine("Books in Shelf:\n");
-            for (int i = 0; i < 5; i++)
-            {
-                shelf[i].Display();
-            }
+            // Display all books on the BookShelf
+            shelf.DisplayAllBooks();
+
+            /* ~ ~ ~ ~ These are only for Decoration Perpose ~ ~ ~ ~ */
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("-----------Programs Execute Sucessfully------------");
+            Console.WriteLine("");
+            Console.WriteLine(" -------END OF PROGRAM---------     ");
+            Console.WriteLine(" Please Click Any Key To Exit.           ");
+            Console.ReadKey();
         }
     }
 }
